3563a676d08c2873212250a264bbc2a776d7eb64
Tass <tasszz2k@gmail.com>
2025-12-29 13:48:51 +0700

    Initial Commit

---
+++ GrammarPolice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GrammarPolice
+//
+//  Created by Tass Dinh on 29/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GrammarPolice/GrammarPoliceApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrammarPoliceApp.swift
+//  GrammarPolice
+//
+//  Created by Tass Dinh on 29/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct GrammarPoliceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
